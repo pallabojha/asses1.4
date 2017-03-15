fa242f642c6773f837cbbdfbe57688e8d8305c7a
pallabojha <ojhapallab@gmail.com>
2017-03-15 16:27:08 +0530

    ascii
    asccii

---
+++ src/assess1/pkg4/Assess14.java
@@ -0,0 +1,28 @@
+
+package assess1.pkg4;
+
+/**
+ *
+ * @author IIS-KOLKATA
+ */
+public class Assess14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     char a;
+        for(int i=65;i<=90; i++)
+        {
+a=(char)i;
+System.out.print(a + "\n");
+
+
+
+
+
+// TODO code application logic here
+    }
+    
+}
+}
